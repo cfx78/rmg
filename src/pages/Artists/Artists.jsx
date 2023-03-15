eea31b05d9ec3c884697e63c641126c58cfad809
cfx78 <104265674+cfx78@users.noreply.github.com>
2023-03-15 00:50:24 -0500

    Importing images

--- src/pages/Artists/Artists.jsx
+++ src/pages/Artists/Artists.jsx
@@ -1,34 +1,37 @@
 import Modal from '../../components/Modal';
 import './Artists.css';
-
+import mtee from '../../../public/assets/mtee_43.jpg';
+import blackLogo from '../../../public/assets/Real_Muziq_Group_Logo_Black.png';
+import blueLogo from '../../../public/assets/Real_Muziq_Group_Logo.jpg';
+import coco from '../../../public/assets/cc_2.jpg';
 const Artists = () => {
 	return (
 		<section id="Artists" className="Artists">
 			<div className="artists--modals grid grid-cols-2 max-w-[100vw] h-full place-items-center mr-9 sm:mr-0">
 				<Modal
 					name="MTee Taylor"
-					img="public\assets\mtee_43.JPG"
+					img={mtee}
 					yInit={{ y: -200 }}
 					yView={{ y: 0 }}
 				/>
 
 				<Modal
 					name="Wop on The Beat"
-					img="public\assets\Real_Muziq_Group_Logo_Black.png"
+					img={blackLogo}
 					yInit={{ y: -200 }}
 					yView={{ y: 0 }}
 				/>
 
 				<Modal
 					name="Cocoa"
-					img="public\assets\cc_2.JPG"
+					img={coco}
 					yInit={{ y: 100 }}
 					yView={{ y: 0 }}
 				/>
 
 				<Modal
 					name="OchoFrom60"
-					img="public\assets\Real_Muziq_Group_Logo.jpg"
+					img={blueLogo}
 					yInit={{ y: 100 }}
 					yView={{ y: 0 }}
 				/>
